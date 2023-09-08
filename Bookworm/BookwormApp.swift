6a3cb9c403fc1700116561d99da287de67f30662
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-09-08 11:42:25 +0530

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Lahiru Amarasooriya on 2023-09-08.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
